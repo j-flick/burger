d2f71248e91906cf0a3981b697c63a781b18c235
Justin Flick <jflick84@gmail.com>
2017-12-02 20:12:33 -0500

    basic app and db setup

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers
+	(burger_name, devoured)
+VALUES
+	("50/50 Burger", false),
+	("Luther Burger", false),
+	("Jucy Lucy", false),
+	("Carolina Burger", false),
+	("Buffalo Burger", false);
+
+
+SELECT * FROM burgers;
